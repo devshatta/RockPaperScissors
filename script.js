74aa991915af01de73302d8bcca04045e2d78a26
Ahmed Shatta <dev.shatta@gmail.com>
2022-09-23 22:09:49 +0800

    create the main game function

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let choices = ['rock', 'paper', 'scissor'];
 
+let playerScore = 0;
+let compScore = 0;
+
 
 // A compact form of the getComputerChoice function
 
@@ -16,6 +19,11 @@ function getComputerChoice(){
     return compChoice;
 }
 
+let playerSelection;
+let computerSelection;
+// let playerSelection = prompt("Write down your choice 'rock,paper or scissor'").toLowerCase();
+// let computerSelection = getComputerChoice();
+
 // console.log(getComputerChoice());
 
 //Rock beats scissors
@@ -27,35 +35,54 @@ function playRound(playerSelection, computerSelection){
     let result;
     let message;
 
+    playerSelection = prompt("Write down your choice 'rock,paper or scissor'").toLowerCase();
+    computerSelection = getComputerChoice();
+    
     if (playerSelection == "rock" || playerSelection == "paper" || playerSelection == "scissor"){
         if (playerSelection == computerSelection){
-            result = "ties";
-        } else if (playerSelection == "rock" && computerSelection == "paper"){
-            result = "lose";
-        } else if (playerSelection == "paper" && computerSelection == "scissor"){
-            result = "lose";
-        } else if (playerSelection == "scissor" && computerSelection == "rock"){
-            result = "lose";
+            result = 0;
+            message = `Equal! \n${playerSelection} ties with ${computerSelection}`;
+        } else if (
+            (playerSelection == "rock" && computerSelection == "paper") ||
+            (playerSelection == "paper" && computerSelection == "scissor") ||
+            (playerSelection == "scissor" && computerSelection == "rock")){
+            result = -1;
+            message = `You lost the round! \n${playerSelection} is beaten by ${computerSelection}`;
+            compScore++
         } else {
-            result = "win";
+            result = 1;
+            message = `You won the round! \n${playerSelection} beats ${computerSelection}`;
+            playerScore++
         }
     } else {
         result = "error";
-    }
-    
-    if (result == "ties"){
-        message = `Equal! \n${playerSelection} ties with ${computerSelection}`;
-    } else if (result == "error"){
         message = `Error! \n${playerSelection} isn't a valid choice`;
-    } else if (result == "lose"){
-        message = `You lost the round! \n${playerSelection} is beaten by ${computerSelection}`;
-    } else {
-        message = `You won the round! \n${playerSelection} beats ${computerSelection}`;
     }
-
-    return message;
+    // console.log(message);
+    console.log(`${message}\nYour score : ${playerScore} \nComputer score : ${compScore}`);
+    // return result;
+    
 }
 
-const playerSelection = prompt("Write down your choice 'rock,paper or scissor'").toLowerCase();
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+function game(){
+    while ((playerScore < 5) && (compScore <5)){
+        playRound();
+    } if (playerScore < compScore){
+        return "YOU LOST THE GAME :(";
+    } else {
+        return "YOU WON THE GAME:)";
+    }
+    }
+
+    // if (playerScore < compScore){
+    //     return "YOU LOST THE GAME :(";
+    // } else if (playerScore > compScore){
+    //     return "YOU WON THE GAME:)";
+    // } else {
+    //     return "NO WINNERS OR LOSERS! PLAY AGAIN"
+    // }
+
+console.log(game());
+// console.log(playRound(playerSelection,computerSelection));
+// console.log(message);
+// console.log(compScore);
